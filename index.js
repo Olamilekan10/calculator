d5f2da4a56b56e41b53d074f91e6c6a16dd2fe33
olamilekan <issaabubakry@gmail.com>
2022-05-30 12:31:08 +0100

    Also forming an array of operators,then replace all numbers and dot with empty string and then split

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ clear = document.getElementById('clear'), // clear button
 resultDisplayed = false; // flag to keep an eye on what output is displayed
 
 // adding click handlers to number buttons
-for (var i = 0; i < number.length; i++) {
+for (let i = 0; i < number.length; i++) {
   number[i].addEventListener("click", function(e) {
 
     // storing current input string and its last character in variables - used later
-    var currentString = input.innerHTML;
-    var lastChar = currentString[currentString.length - 1];
+    let currentString = input.innerHTML;
+    let lastChar = currentString[currentString.length - 1];
 
     // if result is not diplayed, just keep adding
     if (resultDisplayed === false) {
@@ -35,12 +35,12 @@ for (var i = 0; i < number.length; i++) {
 };
 
 // adding click handlers to number buttons
-for (var i = 0; i < operator.length; i++) {
+for (let i = 0; i < operator.length; i++) {
     operator[i].addEventListener("click", function(e) {
   
       // storing current input string and its last character in variables - used later
-      var currentString = input.innerHTML;
-      var lastChar = currentString[currentString.length - 1];
+      let currentString = input.innerHTML;
+      let lastChar = currentString[currentString.length - 1];
   
       // if last character entered is an operator, replace it with the currently pressed one
       if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
@@ -61,14 +61,14 @@ for (var i = 0; i < operator.length; i++) {
     result.addEventListener("click", function() {
 
     // this is the string that we will be processing eg. -10+26+33-56*34/23
-    var inputString = input.innerHTML;
+    let inputString = input.innerHTML;
   
     // forming an array of numbers. eg for above string it will be: numbers = ["10", "26", "33", "56", "34", "23"]
-    // var numbers = inputString.split(/\+|\-|\×|\÷/g);
+    let numbers = inputString.split(/\+|\-|\×|\÷/g);
   
     // forming an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
     // first we replace all the numbers and dot with empty string and then split
-    var operators = inputString.replace(/[0-9]|\./g, "").split("");
+    let operators = inputString.replace(/[0-9]|\./g, "").split("");
   
     console.log(inputString);
     console.log(operators);
@@ -80,14 +80,14 @@ for (var i = 0; i < operator.length; i++) {
     // as we move we are alterning the original numbers and operators array
     // the final element remaining in the array will be the output
   
-    var divide = operators.indexOf("÷");
+    let divide = operators.indexOf("÷");
     while (divide != -1) {
       numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
       operators.splice(divide, 1);
       divide = operators.indexOf("÷");
     }
   
-    var multiply = operators.indexOf("×");
+    let multiply = operators.indexOf("×");
     while (multiply != -1) {
       numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
       operators.splice(multiply, 1);
@@ -101,7 +101,7 @@ for (var i = 0; i < operator.length; i++) {
       subtract = operators.indexOf("-");
     }
   
-    var add = operators.indexOf("+");
+    let add = operators.indexOf("+");
     while (add != -1) {
       // using parseFloat is necessary, otherwise it will result in string concatenation :)
       numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
