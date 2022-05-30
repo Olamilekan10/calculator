2d600d17765137f59e9266869682bae8e8abf7bb
olamilekan <issaabubakry@gmail.com>
2022-05-30 12:15:45 +0100

    Trying to carry out a function of allowing multiple calculation at a time with operators and numbers

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ for (var i = 0; i < operator.length; i++) {
   // on click of 'equal' button
     result.addEventListener("click", function() {
 
-    // this is the string that we will be processing eg. -10+26+33-56*34/23
+    this is the string that we will be processing eg. -10+26+33-56*34/23
     var inputString = input.innerHTML;
   
     // forming an array of numbers. eg for above string it will be: numbers = ["10", "26", "33", "56", "34", "23"]
